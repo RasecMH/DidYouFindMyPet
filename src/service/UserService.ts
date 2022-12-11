1cde7475e27890a20486292667158c9f8cd3a963
RasecMH <rasecmh0@gmail.com>
2022-12-11 14:26:31 -0300

    feat[UserService]: conflic error

--- src/service/UserService.ts
+++ src/service/UserService.ts
@@ -1,10 +1,16 @@
+import { StatusCodes } from 'http-status-codes';
 import User from '../database/models/User';
 import { IUser, IUserRegister } from '../interfaces/UserInterface';
+import HttpError from '../utils/httpError';
 
 export default class UserService {
   model = User;
 
   async create(data: IUserRegister): Promise<IUser> {
+    const hasUser = await this.model.findOne({ where: { email: data.email } });
+    if (hasUser) {
+      throw new HttpError(StatusCodes.CONFLICT, 'email unavailable');
+    }
     return this.model.create({ ...data });
   }
 }
