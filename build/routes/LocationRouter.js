3f52274ca5acdb6873bbcec7e0ffe5cf4da38c6d
RasecMH <rasecmh0@gmail.com>
2023-02-09 01:06:16 -0300

    fix location route

--- build/routes/LocationRouter.js
+++ build/routes/LocationRouter.js
@@ -6,10 +6,11 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = require("express");
 const validateCreateLocationFields_1 = __importDefault(require("../middleware/validateCreateLocationFields"));
 const LocationController_1 = __importDefault(require("../controller/LocationController"));
+const validateToken_1 = __importDefault(require("../middleware/validateToken"));
 const router = (0, express_1.Router)();
 const controller = new LocationController_1.default();
 router.post('/create', validateCreateLocationFields_1.default, controller.create);
-router.get('/:id', controller.findById);
-router.get('/:petId', controller.findAllByPetId);
+router.get('/:id', validateToken_1.default, controller.findById);
+router.get('/all/:petId', validateToken_1.default, controller.findAllByPetId);
 exports.default = router;
 //# sourceMappingURL=LocationRouter.js.map
--- src/routes/LocationRouter.ts
+++ src/routes/LocationRouter.ts
@@ -1,12 +1,13 @@
 import { Router } from 'express';
 import validateCreateLocationFields from '../middleware/validateCreateLocationFields';
 import LocationController from '../controller/LocationController';
+import validateUserToken from '../middleware/validateToken';
 
 const router = Router();
 const controller = new LocationController();
 
 router.post('/create', validateCreateLocationFields, controller.create);
-router.get('/:id', controller.findById);
-router.get('/:petId', controller.findAllByPetId);
+router.get('/:id', validateUserToken, controller.findById);
+router.get('/all/:petId', validateUserToken, controller.findAllByPetId);
 
 export default router;
